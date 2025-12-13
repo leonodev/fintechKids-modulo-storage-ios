0301fd1c95278033f953bb7951480cdfb1653dc9
Fredy Leon <fredy.leon@ua.la.com>
2025-12-13 09:00:56 +0100

    first commit

---
+++ Demo/FHKStorageDemo/FHKStorageDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FHKStorageDemo
+//
+//  Created by Fredy Leon on 13/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo/FHKStorageDemo/FHKStorageDemo/FHKStorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FHKStorageDemoApp.swift
+//  FHKStorageDemo
+//
+//  Created by Fredy Leon on 13/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FHKStorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
