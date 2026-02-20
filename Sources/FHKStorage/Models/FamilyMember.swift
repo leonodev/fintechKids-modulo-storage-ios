27a3ea1139b49f0ade691f5c8731cf20dd3c1f04
Fredy Leon <fredy.leon@ua.la.com>
2026-02-21 00:06:57 +0100

    add Equatable

--- Sources/FHKStorage/Models/FamilyMember.swift
+++ Sources/FHKStorage/Models/FamilyMember.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct FamilyMember: Codable, Identifiable, Hashable {
+public struct FamilyMember: Codable, Identifiable, Hashable, Equatable {
     // Identificador de Supabase (Opcional para nuevos registros)
     public var id: Int?
     let email: String
