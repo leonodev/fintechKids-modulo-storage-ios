2d7fb11d7f2759a7b5f4ba80b7345f125d609842
Fredy Leon <fredy.leon@ua.la.com>
2026-02-25 07:28:36 +0100

    remove property

--- Sources/FHKStorage/Models/FamilyMember.swift
+++ Sources/FHKStorage/Models/FamilyMember.swift
@@ -17,7 +17,6 @@ public struct FamilyMember: Codable, Identifiable, Hashable, Equatable {
     public let member_name: String
     public let avatar_name: String
     // Propiedades de conveniencia para la UI (No se guardan en la DB)
-    public let identification_uuid: String? = nil
     public var iconName: String = ImageSystem.trash.name
     
     public init(id: Int? = nil,
@@ -37,6 +36,5 @@ public struct FamilyMember: Codable, Identifiable, Hashable, Equatable {
         case email_parent
         case member_name
         case avatar_name
-        case identification_uuid
     }
 }
