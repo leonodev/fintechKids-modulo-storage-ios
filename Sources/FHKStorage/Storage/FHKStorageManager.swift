787a3d0a4a8a612fabbd815e7bc77dcd44a45c58
Fredy Leon <feleon@knowmadmood.com>
2026-07-08 08:59:57 +0200

    add FHKStorageManager

--- Sources/FHKStorage/Storage/FHKStorageManager.swift
+++ Sources/FHKStorage/Storage/FHKStorageManager.swift
@@ -10,27 +10,6 @@ import LocalAuthentication
 import FHKDomain
 import FHKUtils
 
-public struct FHKStorageManager: Sendable {
-    
-    // MARK: - UserDefaults
-    public var saveUserDefaultsData: @Sendable (Data, String) async throws -> Void = { _, _ in }
-    public var readUserDefaultsData: @Sendable (String) async throws -> Data? = { _ in nil }
-    public var deleteUserDefaultsData: @Sendable (String) async throws -> Void = { _ in }
-    
-    // MARK: - Keychain
-    public var saveKeychainData: @Sendable (Data, String, Bool) throws -> Void = { _, _, _ in }
-    public var readKeychainData: @Sendable (String, String?) throws -> Data? = { _, _ in nil }
-    public var deleteKeychainData: @Sendable (String) throws -> Void = { _ in }
-    public var containsKeychainKey: @Sendable (String) -> Bool = { _ in false }
-    public var clearAllKeychainData: @Sendable () throws -> Void = {}
-    
-    // MARK: - Sistema / Hardware
-    public var isBiometryAvailableAction: @Sendable () -> Bool = { false }
-    public var existsKeyAction: @Sendable (String) -> Bool = { _ in false }
-
-    public init() {}
-}
-
 // MARK: - UserDefaults API
 public extension FHKStorageManager {
     
